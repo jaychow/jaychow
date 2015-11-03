0d66c705bc29cd3e136ed8536870be6b3430f969
jaychow <jay.chow@katanaconnect.com>
2015-11-02 16:57:38 -0800

    content update

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -11,31 +11,7 @@
       <div class="row">
         <div class="col s12">
           <a id="logo-container" href="{{ url('/') }}" class="brand-logo">Jay Chow</a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="{{ url('cards') }}">Cards</a></li>
-            <li><a href="#">Link 2</a></li>
-            <!-- Dropdown Trigger -->
-            <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-          </ul>
-          <ul id="nav-mobile" class="side-nav">
-            <li><a href="{{ url('cards') }}">Cards</a></li>
-            <li><a href="#!">Link 2</a></li>
-            <!-- Dropdown Trigger -->
-            <li class="no-padding">
-              <ul class="collapsible collapsible-accordion">
-                <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Dropdown</a>
-                  <div class="collapsible-body">
-                    <ul class="grey lighten-3">
-                      <li><a href="#!">One</a></li>
-                      <li><a href="#!">Two</a></li>
-                      <li class="divider"></li>
-                      <li><a href="#!">Three</a></li>
-                    </ul>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
+
           <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
         </div>
       </div>
