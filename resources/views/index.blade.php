731b0f2d28e583dbbad84522f8a238dadad2322d
jaychow <jay.chow@katanaconnect.com>
2015-12-11 16:09:35 -0800

    content update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,27 +6,23 @@
 @endsection
 
 @section('content')
-  <main>
-    <div class="section no-pad-bot" id="index-banner">
-      <div class="row">
-        <div class="col s12">
-          <div class="content">
-            <div class="title">Hello, my name is Jay Chow. I am an ICAM graduate from UCSD, Senior Web Developer at Katana, with skills in multimedia production, experience in data visualization on massive cultural data, and have a passion for entrepreneurship.</div>
-          </div>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <div class="center-align">
+          <h1><b>Under Construction</b></h1>
+          <h5>In the mean time, learn more <a href="http://about.me/jaychow">about.me</a>!</h5>
+          <h3><i class="fa fa-thumbs-up"></i></h3>
         </div>
-      </div><!--  /.container  -->
-    </div><!-- /.section -->
-  </main>
+      </div>
+    </div>
+  </div>
+</main>
   @endsection
 @section('before-scripts')
 @endsection
 
 @section('after-scripts')
-  <script>
-  $(document).ready(function(){
-      $('.collapsible').collapsible({
-        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-      });
-    });
-  </script>
+
 @endsection
